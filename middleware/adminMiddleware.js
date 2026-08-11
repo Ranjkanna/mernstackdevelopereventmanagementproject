c9f4e35cf8433d495e2e76912dbbe4172fa825b8
Ranchani <ranchani73c@gmail.com>
2026-08-12 04:46:02 +0530

    Restructure backend into server folder

--- middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = function adminOnly(req, res, next) {
-  if (req.user && req.user.isAdmin) {
-    return next();
-  }
-  return res.status(403).json({ message: "Admin access only" });
-};
